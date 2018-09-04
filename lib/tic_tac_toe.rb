47d84935b9e3ae24d1e1a7326458e7c0f3a50ae1
Samuel Cornet <samuel@noukod.com>
2018-09-04 14:34:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,3 +63,13 @@ def turn(board)
 end
 display_board(board)
 end
+
+def turn_count(board)
+  counter = 0 
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1 
+    end 
+  end
+  counter
+end
