1000e752c7502c75ae572e12187f96d6599869e4
Samuel Cornet <samuel@noukod.com>
2018-09-04 14:34:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,11 +65,11 @@ display_board(board)
 end
 
 def turn_count(board)
-  counter = 0 
+  counter = 0
   board.each do |turn|
     if turn == "X" || turn == "O"
-      counter += 1 
-    end 
+      counter += 1
+    end
   end
   counter
 end
